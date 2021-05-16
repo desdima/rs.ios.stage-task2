528a6cc0c2a6be5d3d46470f39d9922074f347f6
desdima <des.dima@gmail.com>
2021-05-17 00:20:35 +0400

    feat: removed unnecessary code

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -13,17 +13,6 @@
     
     NSMutableArray *sortArray = [array mutableCopy];
     [sortArray sortUsingSelector:@selector(compare:)];
-    /*
-    NSMutableArray *sortArrayWithoutDoubles = [[NSMutableArray alloc] init];
-    [sortArrayWithoutDoubles insertObject:sortArray[0] atIndex:0];
-    
-    int j = 1;
-    for (int i = 1; i < sortArray.count; i++) {
-        if (sortArray[i - 1] != sortArray[i]) {
-            [sortArrayWithoutDoubles insertObject:sortArray[i] atIndex:j];
-            j++;
-        }
-    }*/
     
     for (int i = 0; i < sortArray.count; i++) {
         long i_num = [sortArray[i] integerValue];
