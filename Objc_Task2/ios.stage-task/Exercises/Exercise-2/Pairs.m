8acd58a94a6f979f54894932cccaf59115c79c4a
desdima <des.dima@gmail.com>
2021-05-17 00:17:21 +0400

    fix: Exercises 2 & 4. Change applications logic

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -4,12 +4,16 @@
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
     
+    if (array.count == 0) return 0;
+    
     long pairNum = [number integerValue];
     int countPairs = 0;
     
+    
+    
     NSMutableArray *sortArray = [array mutableCopy];
     [sortArray sortUsingSelector:@selector(compare:)];
-    
+    /*
     NSMutableArray *sortArrayWithoutDoubles = [[NSMutableArray alloc] init];
     [sortArrayWithoutDoubles insertObject:sortArray[0] atIndex:0];
     
@@ -19,12 +23,12 @@
             [sortArrayWithoutDoubles insertObject:sortArray[i] atIndex:j];
             j++;
         }
-    }
+    }*/
     
-    for (int i = 0; i < sortArrayWithoutDoubles.count; i++) {
-        long i_num = [sortArrayWithoutDoubles[i] integerValue];
-        for (int j = 0; j < sortArrayWithoutDoubles.count; j++) {
-            if (i_num == [sortArrayWithoutDoubles[j] integerValue] + pairNum) countPairs++;
+    for (int i = 0; i < sortArray.count; i++) {
+        long i_num = [sortArray[i] integerValue];
+        for (int j = i + 1; j < sortArray.count; j++) {
+            if (i_num == [sortArray[j] integerValue] + pairNum) countPairs++;
         }
     }
     
--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -5,92 +5,73 @@
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
     
     if ((array == nil) || (array.count == 0)) return @[];
-    NSMutableArray *unionArray;
-    NSMutableArray *unionArrayInt = [[NSMutableArray alloc] init];
-    NSMutableArray *unionArrayStr = [[NSMutableArray alloc] init];
+    NSMutableArray *newUnionArrayInt = [NSMutableArray new];
+    NSMutableArray *newUnionArrayStr = [NSMutableArray new];
     
     for (int i = 0; i < array.count; i++) {
         if ([array[i] isKindOfClass:[NSArray class]]) {
-            if ([array[i][1] isKindOfClass:[NSNumber class]]) {
-                unionArray = unionArrayInt;
+            if (array[i].count != 0) {
+            if ([array[i][0] isKindOfClass:[NSNumber class]]) {
+                for (int j = 0; j < array[i].count; j++) {
+                    [newUnionArrayInt insertObject:array[i][j] atIndex:newUnionArrayInt.count];
+                }
             }
             else {
-                unionArray = unionArrayStr;
-            }
-            for (int j = 0; j < array[i].count; j++) {
-                [unionArray insertObject:array[i][j] atIndex:unionArray.count];
-            }
-        }
-        else {
-            //оформить отдельным методом
-                int numCount = 0;
-                int numStr = 0;
-                for (int i = 0; i < array.count; i++) {
-                    if ([array[i] isKindOfClass:[NSNumber class]]) {
-                        numCount++;
-                    }
-                    if ([array[i] isKindOfClass:[NSString class]]) {
-                        numStr++;
-                    }
+                for (int j = 0; j < array[i].count; j++) {
+                    [newUnionArrayStr insertObject:array[i][j] atIndex:newUnionArrayStr.count];
                 }
-                if ((numCount != 0) && (numStr != 0)) return @[];
-                
-                unionArray = [array mutableCopy];
+            }
+            
+            }}
+        else {
+            return @[];
             }
     }
-    [unionArray sortUsingSelector:@selector(compare:)];    
     
-    NSMutableArray *unionArray3 = [[NSMutableArray alloc] init];;
     NSMutableArray *unionArrayInt3 = [[NSMutableArray alloc] init];
     NSMutableArray *unionArrayStr3 = [[NSMutableArray alloc] init];
     
     
-    
-    [unionArray3 insertObject:unionArray[0] atIndex:0];
     int j = 1;
-    for (int i = 1; i < unionArray.count; i++) {
-        if (unionArray[i - 1] != unionArray[i]) {
-            [unionArray3 insertObject:unionArray[i] atIndex:j];
-            j++;
-        }
-    }
-    unionArray = unionArray3;
-    
-    if (unionArrayInt.count != 0) {
-        [unionArrayInt sortUsingSelector:@selector(compare:)];
-        [unionArrayInt3 insertObject:unionArrayInt[0] atIndex:0];
+    if (newUnionArrayInt.count != 0) {
+        [newUnionArrayInt sortUsingSelector:@selector(compare:)];
+        [unionArrayInt3 insertObject:newUnionArrayInt[0] atIndex:0];
         j = 1;
-        for (int i = 1; i < unionArrayInt.count; i++) {
-            if (unionArrayInt[i - 1] != unionArrayInt[i]) {
-                [unionArrayInt3 insertObject:unionArrayInt[i] atIndex:j];
+        for (int i = 1; i < newUnionArrayInt.count; i++) {
+            if (newUnionArrayInt[i - 1] != newUnionArrayInt[i]) {
+                [unionArrayInt3 insertObject:newUnionArrayInt[i] atIndex:j];
                 j++;
             }
         }
-        unionArrayInt = unionArrayInt3;
+        newUnionArrayInt = unionArrayInt3;
         
     }
     
-    if (unionArrayStr.count != 0) {
-        [unionArrayStr sortUsingSelector:@selector(compare:)];
-        [unionArrayStr3 insertObject:unionArrayStr[0] atIndex:0];
+    if (newUnionArrayStr.count != 0) {
+        [newUnionArrayStr sortUsingSelector:@selector(compare:)];
+        [unionArrayStr3 insertObject:newUnionArrayStr[0] atIndex:0];
         j = 1;
-        for (int i = 1; i < unionArrayStr.count; i++) {
-            if (unionArrayStr[i - 1] != unionArrayStr[i]) {
-                [unionArrayStr3 insertObject:unionArrayStr[i] atIndex:j];
+        for (int i = 1; i < newUnionArrayStr.count; i++) {
+            if (newUnionArrayStr[i - 1] != newUnionArrayStr[i]) {
+                [unionArrayStr3 insertObject:newUnionArrayStr[i] atIndex:j];
                 j++;
             }
         }
-        unionArrayStr = unionArrayStr3;
+        newUnionArrayStr = unionArrayStr3;
     }
     
     NSMutableArray *unionArrayInt2 = [[NSMutableArray alloc] init];
     
-    if ((unionArrayInt.count != 0) && (unionArrayStr.count != 0)) {
-        [unionArrayInt2 insertObject:unionArrayInt atIndex:0];
+    if ((newUnionArrayInt.count != 0) && (newUnionArrayStr.count == 0)) return newUnionArrayInt;
+    if ((newUnionArrayInt.count == 0) && (newUnionArrayStr.count != 0)) return newUnionArrayStr;
+    
+    
+    if ((newUnionArrayInt.count != 0) && (newUnionArrayStr.count != 0)) {
+        [unionArrayInt2 insertObject:newUnionArrayInt atIndex:0];
         
         NSSortDescriptor *descriptor = [[NSSortDescriptor alloc] initWithKey:@"self" ascending:NO];
         NSArray *descriptors = [NSArray arrayWithObject: descriptor];
-        NSArray *reverseOrder = [unionArrayStr sortedArrayUsingDescriptors:descriptors];
+        NSArray *reverseOrder = [newUnionArrayStr sortedArrayUsingDescriptors:descriptors];
         
         [unionArrayInt2 insertObject:reverseOrder atIndex:1];
         
@@ -98,7 +79,7 @@
         
     }
 
-    return unionArray;
+    return @[];
 }
 
 @end
